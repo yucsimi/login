87771dc514ff8b29a8f18e43f8b50da6e258fab7
Duwan <yucsimigliced14@gmail.com>
2022-02-26 10:01:54 -0800

    warnings

--- src/Componentes/ModalFormulario.js
+++ src/Componentes/ModalFormulario.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
+import { Modal, ModalBody } from 'reactstrap'
 
 import imagen from "../assets/d.png"
 export default function ModalFormulario({ modal, handleSubmit }) {
